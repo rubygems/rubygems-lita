d273f4ee7f6a311d463efcd64c0538edfda5a9ee
David Radcliffe <radcliffe.david@gmail.com>
2015-06-27 12:41:47 -0400

    enable debug logging

--- lita_config.rb
+++ lita_config.rb
@@ -18,7 +18,7 @@ Lita.configure do |config|
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
   # Messages at the selected level and above will be logged.
-  config.robot.log_level = :info
+  config.robot.log_level = :debug
 
   # An array of user IDs that are considered administrators. These users
   # the ability to add and remove other users from authorization groups.
